f1331dbecb6cdbd8703cbd05df3b2dea444be545
arte <ardjanve@gmail.com>
2020-05-30 16:07:00 +0200

    test

--- push.sh
+++ push.sh
@@ -1,4 +1,9 @@
-git add . 
-git commit -m $1
-git pull upstream master
-git push upstream master
+if [ $# -eq 0 ]
+then
+	echo "please provide a commit message"
+else
+	git add . 
+	git commit -m $1
+	git pull upstream master
+	git push upstream master
+fi
